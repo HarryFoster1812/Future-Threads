b3764bc1cbe7c614369df83c563dc263aaa3e8c1
Wren L. Alexander <wren.alexander@student.manchester.ac.uk>
2025-04-05 11:57:20 +0100

    meow

--- src/scripts/textGeneration.js
+++ src/scripts/textGeneration.js
@@ -2,7 +2,7 @@
 /* 
 
 	category: string, what type of thing to generate
-	options: {optimism: int 0-10, genres: {name: string, amount: int 0-10}}
+	options: {optimism: int 0-10, genres: [{name: string, amount: int 0-10}}]
 
 	returns: string of the text
 */
