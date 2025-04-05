9edd8a8a8a25f31e8bb7e275c828ff7bd28e9660
r49769hf <harry.foster-2@student.manchester.ac.uk>
2025-04-05 15:12:04 +0100

    added pages

--- src/App.js
+++ src/App.js
@@ -1,52 +1,24 @@
-import './App.css';
-import Filter from './home/filter.jsx';
-import React, { useState, useEffect } from 'react';
-import Event from './timeline/Event';
-import LoadMoreButton from './timeline/LoadMoreButton';
+import './App.css' 
+import Home from './pages/Home/Home'
+import Event from './pages/Event/EventInformation'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import NoPage from "./pages/NoPage/NoPage";
 
-const App = () => {
-    const [events, setEvents] = useState([]);
-    const [loadedEvents, setLoadedEvents] = useState(0);
-    const eventsPerLoad = 6;
-
-    const allEvents = Array.from({ length: 100 }, (_, i) => ({
-        title: `Event Title ${i + 1}`,
-        date: `2025-0${(i % 12) + 1}-01`,
-        image: i % 3 === 0 ? `https://via.placeholder.com/150?text=Event+${i + 1}` : null,
-        url: i % 4 === 0 ? `https://example.com/event-${i + 1}` : null,
-        content: `Description for event ${i + 1}. Lorem ipsum dolor sit amet...`,
-    }));
-
-    const loadEvents = () => {
-        setEvents((prevEvents) => [
-            ...prevEvents,
-            ...allEvents.slice(loadedEvents, loadedEvents + eventsPerLoad),
-        ]);
-        setLoadedEvents(loadedEvents + eventsPerLoad);
-    };
-
-    useEffect(() => {
-        loadEvents();
-    }, []);
+function App() {
 
     return (
-        <div className="flex items-center justify-center flex-col bg-black p-4">
-            <Filter/>
-
-            {/* Timeline Container */}
-            <div className="relative w-full md:w-2/3">
-                {/* Vertical Timeline Line */}
-                <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-1 bg-gray-300 h-full z-0"></div>
-
-                {events.map((event, index) => (
-                    <Event key={index} event={event} side={index % 2 === 0 ? 'left' : 'right'} />
-                ))}
-            </div>
-
-            <LoadMoreButton onClick={loadEvents} />
-
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="event/:info" element={<Event/>}/>
+                    <Route path="*" element={<NoPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
-}
 
-export default App;
+}
+export default App
+
