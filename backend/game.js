e8187b5a601fbfa535e4e66b442872fe6788aa76
Chris Harris <christian.harris-3@student.manchester.ac.uk>
2025-04-05 21:19:53 +0100

    swapped for new data

---
+++ backend/game.js
@@ -0,0 +1,24 @@
+
+export default class Game {
+    constructor() {
+        this.stats = {
+            economy: 0.5,
+            environment: 0.5,
+            politics: 0.5,
+            military: 0.5,
+            qualityOfLife: 0.5
+        }
+    }
+    generateCards() {
+
+    }
+    selectCard() {
+
+    }
+    generateEvents() {
+
+    }
+    getStats() {
+        return this.stats
+    }
+}
